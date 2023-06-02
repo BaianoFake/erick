8e65792682eeaff7e6462d52d99082fa4165167e
erick freire <erickfreire616@gmail.com>
2023-06-02 15:21:42 -0300

    movimentação eixo x

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,10 +1,11 @@
 import pygame
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING, DEFAULT_TYPE, SHIELD_TYPE, HAMMER_TYPE, TIME_TYPE, DUCKING_SHIELD, JUMPING_SHIELD, RUNNING_SHIELD, DUCKING_HAMMER, JUMPING_HAMMER, RUNNING_HAMMER
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING, DEFAULT_TYPE, SHIELD_TYPE, HAMMER_TYPE, TIME_TYPE, DUCKING_SHIELD, JUMPING_SHIELD, RUNNING_SHIELD, DUCKING_HAMMER, JUMPING_HAMMER, RUNNING_HAMMER, SCREEN_WIDTH
 
 
 X_POS = 80
+X_VEL = 10
 Y_POS = 310
 JUMP_VEL = 8.5
 
@@ -41,6 +42,14 @@ class Dinosaur(Sprite):
         else:
             self.run()
 
+        if user_input[pygame.K_LEFT] or user_input[pygame.K_a]:
+            if self.dino_rect.x > 0:
+                self.dino_rect.x -= X_VEL
+        elif user_input[pygame.K_RIGHT] or user_input[pygame.K_d]:
+            if self.dino_rect.right < SCREEN_WIDTH:
+                self.dino_rect.x += X_VEL
+
+
         if (user_input[pygame.K_UP] or user_input[pygame.K_w]) and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
@@ -54,15 +63,13 @@ class Dinosaur(Sprite):
             self.dino_duck = False
             self.dino_run = True
         elif (user_input[pygame.K_DOWN] or user_input[pygame.K_s]) and self.dino_jump:
-            self.jump_vel -= 1
+            self.jump_vel -= 0.4
 
         if self.step_index >= 10:
             self.step_index = 0
 
     def run(self):
         self.image = RUN_IMG[self.type][self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
         self.step_index += 1
 
@@ -79,8 +86,6 @@ class Dinosaur(Sprite):
 
     def duck(self):
         self.image = DUCK_IMG[self.type][self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS + 40
         self.step_index += 1
         self.dino_duck = False
