32f5b1814cc8c19e7c4f083d972a7752a2ed5b2d
erick freire <erickfreire616@gmail.com>
2023-06-02 17:07:13 -0300

    ajuste de valor

--- dino_runner/components/power_ups/power_up.py
+++ dino_runner/components/power_ups/power_up.py
@@ -11,7 +11,7 @@ class PowerUp(Sprite):
         self.rect.x = SCREEN_WIDTH + random.randint(800, 1000)
         self.rect.y = random.randint(125, 175)
         self.start_time = 0
-        self.duration = random.randint(10, 15)
+        self.duration = random.randint(5, 10)
 
     def update(self, game_speed, power_ups):
         self.rect.x -= game_speed
